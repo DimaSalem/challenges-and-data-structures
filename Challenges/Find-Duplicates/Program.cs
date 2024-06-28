141de8c42457e7727c653bfaf8d112a25568db4e
DimaSalem <demamohamad1998@gmail.com>
2024-06-28 19:43:15 +0300

    ADD cc5 solution

---
+++ Challenges/Find-Duplicates/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+
+namespace cc1
+{
+    internal class Program
+    {
+      
+        //Find Duplicates
+        static int[] copyArray(int[] arrTo, int[] arrFrom)
+        {
+            for (int i = 0; i < arrTo.Length; i++)
+            {
+                arrTo[i] = arrFrom[i];
+            }
+            return arrTo;
+        }
+        static int[] FindDuplicates(int[] arr)
+        {
+            int[] tempDuplicates= new int[arr.Length];
+            int deplicatesCount = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i+1; j < arr.Length; j++)
+                {
+                    if (arr[i] == arr[j])
+                        tempDuplicates[deplicatesCount++] = arr[j];
+                }
+            }
+            int[] arrDuplicates= new int[deplicatesCount];
+            copyArray(arrDuplicates, tempDuplicates);
+            return arrDuplicates;
+        }
+        static void Main(string[] args)
+        {
+
+
+            int[] arr = FindDuplicates(new int[] { 100, 1, 2,2,5, 5, 3, 4, 4, 100});
+
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
