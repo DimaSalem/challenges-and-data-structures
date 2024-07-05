8586b99ea635f7821a0a85b796403129bc95958e
DimaSalem <demamohamad1998@gmail.com>
2024-07-05 22:28:38 +0300

    ADD CC7 solution

---
+++ Challenges/Reverse-Words/Readme.md
@@ -0,0 +1,4 @@
+### Challenge Name: Reverse Words
+A function that takes a string and reverse the words in it then return a string with the reversed words.
+
+![Reverse Words](./ReverseWords.PNG)
---
+++ Challenges/Reverse-Words/Reverse-Words/Reverse-Words/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Reverse_Words
+{
+    public class Program
+    {
+        static string[] Split(string input)
+        {
+            List<string> result = new List<string>();
+            string word = "";
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] != ' ')
+                    word += input[i];
+                else if (word != "" && word != " ")
+                {
+                    result.Add(word);
+                    word = "";
+                }
+
+            }
+            if (word != "")
+                result.Add(word);
+            return result.ToArray();
+        }
+        static public string ReverseWords(string input)
+        {
+            string[] splitInput= Split(input);
+            string result = "";
+            for (int i = 0; i < splitInput.Length; i++)
+            {
+                if (i == splitInput.Length - 1)
+                    result += splitInput[splitInput.Length - i - 1];
+                else
+                    result += splitInput[splitInput.Length-i-1] + " ";
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            string  s1 = "csharp is programming language";
+            //foreach (string item in Split(s1))
+            //{
+            //    Console.WriteLine(item);
+            //}
+            Console.WriteLine(ReverseWords(s1));
+
+        }
+    }
+}
